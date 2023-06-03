6564b0a2bce91d051f423e43bdd35d6d014f87cc
maanikg <gognamaanik@gmail.com>
2023-06-03 15:15:39 -0400

    test

--- vacay-away/src/App.js
+++ vacay-away/src/App.js
@@ -28,7 +28,7 @@ function App() {
       <header className="App-body">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload!!.
           {/* <button>Hi</button> */}
         </p>
         <a
