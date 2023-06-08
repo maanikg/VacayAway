b6aac30526861114445783424b969a78583ed27b
maanikg <gognamaanik@gmail.com>
2023-06-08 15:49:05 -0400

    create app from scratch for npm errors workaround

---
+++ vacay-away1/src/App.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+import MapView from './MapView';
+import HomeScreen from './HomeScreen';
+
+export default function App() {
+  return (
+    //  <MapView />
+    <HomeScreen />
+  );
+}
---
+++ vacay-away1/src/App_old.jsx
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const user = {
+    name: 'Vacation Person',
+    location: {
+      city: 'San Francisco',
+      country: 'USA'
+    }
+  }
+
+
+  return (
+    <div className="App">
+      {/* <div className="wrapper">
+        <div className="first">first</div>
+        <div className="second">second</div>
+        <div className="third">third</div>
+      </div> */}
+      <header>
+        <p>Created by {user.name}</p>
+        <p>Location: {user.location.city}, {user.location.country}</p>
+        {/* <React.Button> </React.Button> */}
+        {/* <Button> </Button> */}
+        {/* <button>Hi</button> */}
+      </header>
+      <header className="App-body">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload!!.
+          {/* <button>Hi</button> */}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <footer>
+        <p>Created by {user.name}</p>
+        <p>Location: {user.location.city}, {user.location.country}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
