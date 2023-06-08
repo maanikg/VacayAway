d8b696fc48b03c0cd6f595b4abebdfbedffaeacc
maanikg <gognamaanik@gmail.com>
2023-06-08 14:16:49 -0400

    checking matching passwords

--- vacay-away/src/HomeScreen.jsx
+++ vacay-away/src/HomeScreen.jsx
@@ -1,20 +1,60 @@
+import { useState } from "react";
 export default function HomeScreen() {
+    const [username, setUsername] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+    const [passwordVerify, setPasswordVerify] = useState("");
     function buttonClick() {
-        alert("hi");
+        if (passwordInput != passwordVerify) {
+            alert("Passwords do not match");
+        } else {
+            alert("hi");
+        }
     }
     return (
         <div>
             <h1>HomeScreen</h1>
-            <div style={{ display: 'flex' }}>
-                <h1 style={{ backgroundColor: 'green' }}>Title</h1>
+            <h1 style={{ backgroundColor: 'green' }}>Title</h1>
+
+            <div>
                 <button>Explore!</button>
+            </div>
+            <div>
                 <button>Curate your plan</button>
             </div>
-            <div style={{ display: 'flex' }}>
+            <div>
                 <button >My trips</button>
             </div>
             <div style={{ display: 'flex' }}>
+                <input
+                    name="usernameInput"
+                    type="text"
+                    placeholder="Username"
+                    autoComplete="false"
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input
+                    name="passwordInput"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="false"
+                    onChange={e => setPasswordInput(e.target.value)}
+                />
+                <input
+                    name="passwordVerify"
+                    type="password"
+                    placeholder="Verify Password"
+                    autoComplete="false"
+                    onChange={e => setPasswordVerify(e.target.value)}
+                // onChange={e => {
+                //     if (passwordInput.value != passwordVerify.value) {
+                //         alert("Passwords do not match");
+                //     }
+                // }
+                // }
+                />
+
                 <button onClick={buttonClick} >Log In</button>
+                <div>{username}</div>
             </div>
         </div>
     );
