b00e7a81b235d27aa5bf135fd80bb908653c2011
maanikg <gognamaanik@gmail.com>
2023-06-08 17:11:15 -0400

    second initialization

--- vacay-away2/src/App.jsx
+++ vacay-away2/src/App.jsx
@@ -4,7 +4,7 @@ import HomeScreen from './HomeScreen';
 
 export default function App() {
   return (
-    //  <MapView />
-    <HomeScreen />
+    <MapView />
+    // <HomeScreen />
   );
 }
--- vacay-away2/src/index.js
+++ vacay-away2/src/index.js
@@ -9,8 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App_old /> */}
-    <App />
+    {/* <h1>hi</h1> */}
+    <App_old />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
