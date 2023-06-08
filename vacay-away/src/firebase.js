b4808248d4f9c450eecaf52ede8f94c39e15a689
maanikg <gognamaanik@gmail.com>
2023-06-08 10:50:14 -0400

    initializing work on firebase

--- vacay-away/src/firebase.js
+++ vacay-away/src/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,4 +21,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
 const analytics = getAnalytics(app);
+
+onAuthStateChanged(auth, user => {
+  if (user != null) {
+    console.log('We are authenticated now!')
+  } else {
+    console.log('We are not authenticated')
+  }
+});
