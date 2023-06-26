370a2fa0cc9aaeb377ffb7429d98aee072f2820b
maanikg <gognamaanik@gmail.com>
2023-06-26 16:55:57 -0400

    adding plan gen screen

---
+++ test-router/src/pages/plan/PlanGenerationScreen.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+// import { useState } from "react"
+export default function DestinationSelectedScreen(props) {
+    return (
+        <div
+            style={{
+                display: props.display,
+                backgroundColor: "purple"
+            }}
+        >
+            <div style={{ backgroundColor: "lightblue" }}>
+                <p>You have selected:</p>
+                {
+                    props.destArray.map((city) => {
+                        return (
+                            <div key={city.key}>
+                                <p>{city.city}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div>
+                <div
+                    style={{ display: "flex" }}
+                >
+                    <p>Dates:</p>
+                    <p>{props.departureDate.toString()}</p>
+                    <p>{props.returnDate.toString()}</p>
+                </div>
+                <div
+                    style={{ display: "flex" }}
+                >
+                    <p>When do you want to come back?</p>
+                </div>
+            </div>
+        </div >
+    )
+}
