db5a48c7143c61c401c9c984f92ff9f99d365666
maanikg <gognamaanik@gmail.com>
2023-07-05 15:13:22 -0400

    trying to get return flight data

--- test-router/src/pages/plan/PlanGenerationScreen.jsx
+++ test-router/src/pages/plan/PlanGenerationScreen.jsx
@@ -19,24 +19,21 @@ export default function DestinationSelectedScreen(props) {
     const [departureAirports, setDepartureAirports] = useState([])
     const [departureFlights, setDepartureFlights] = useState([])
     const [cheapestFlight, setCheapestFlight] = useState({})
+    const [returnFlight, setReturnFlight] = useState({})
+
 
     function saveTripData() {
-        const pathRef = ref(db, 'users/' + auth.currentUser.uid + '/trips')
+        saveArrivalTripData()
+        // saveReturnTripData()
+    }
+    function saveArrivalTripData() {
+        const pathRef = ref(db, 'users/' + auth.currentUser.uid + '/trips/departure')
         const newPath = push(pathRef)
+        const departurePath = newPath + '/departure'
         const segments = cheapestFlight.data[0].itineraries[0].segments;
         const lastIndex = segments.length - 1;
         const segmentData = [];
-        console.log(segments)
-        // const lastIndex = cheapestFlight.itineraries[0].segments.length - 1;
-        // set(newPath, {
-        //     // departure: cheapestFlight.itineraries[0].segments[0].departure.iataCode,
-        //     // departureTerminal: cheapestFlight.itineraries[0].segments[0].departure.iataCode,
-        //     // departureDateTime: cheapestFlight.itineraries[0].segments[0].departure.at,
-        //     // destination: cheapestFlight.destinationAirport,
-        //     // returnDate: cheapestFlight.returnDate,
-        //     price: cheapestFlight.price.grandTotal,
-        //     duration: cheapestFlight.itineraries[0].duration
-        // })
+        // console.log(segments)
         segments.forEach((segment, index) => {
             segmentData.push({
                 departure: segment.departure.iataCode,
@@ -51,7 +48,39 @@ export default function DestinationSelectedScreen(props) {
                 aircraft: cheapestFlight.dictionaries.aircraft[segment.aircraft.code],
             });
             if (index === lastIndex) {
-                set(newPath, {
+                set(departurePath, {
+                    segments: segmentData,
+                    price: cheapestFlight.data[0].price.grandTotal,
+                    duration: cheapestFlight.data[0].itineraries[0].duration,
+                    currencyShortForm: cheapestFlight.data[0].price.currency,
+                    currencyLongForm: cheapestFlight.dictionaries.currencies[cheapestFlight.data[0].price.currency],
+                });
+            }
+        });
+    }
+    function saveReturnTripData() {
+        const pathRef = ref(db, 'users/' + auth.currentUser.uid + '/trips')
+        const newPath = push(pathRef)
+        const arrivalPath = newPath + '/arrival'
+        const segments = cheapestFlight.data[0].itineraries[0].segments;
+        const lastIndex = segments.length - 1;
+        const segmentData = [];
+        // console.log(segments)
+        segments.forEach((segment, index) => {
+            segmentData.push({
+                departure: segment.departure.iataCode,
+                departureTerminal: segment.departure.terminal ? segment.departure.terminal : null,
+                departureDateTime: segment.departure.at,
+                destination: segment.arrival.iataCode,
+                destinationTerminal: segment.arrival.terminal ? segment.arrival.terminal : null,
+                destinationDateTime: segment.arrival.at,
+                duration: segment.duration,
+                flightNumber: segment.carrierCode + segment.number,
+                airline: cheapestFlight.dictionaries.carriers[segment.carrierCode],
+                aircraft: cheapestFlight.dictionaries.aircraft[segment.aircraft.code],
+            });
+            if (index === lastIndex) {
+                set(arrivalPath, {
                     segments: segmentData,
                     price: cheapestFlight.data[0].price.grandTotal,
                     duration: cheapestFlight.data[0].itineraries[0].duration,
@@ -133,16 +162,24 @@ export default function DestinationSelectedScreen(props) {
     }
     function getFlight() {
         const departureDate = new Date(props.departureDate);
-        const year = departureDate.getFullYear();
-        const month = departureDate.getMonth() + 1; // add 1 to get month in range 1-12
-        const day = departureDate.getDate();
-        const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+        const depYear = departureDate.getFullYear();
+        const depMonth = departureDate.getMonth() + 1; // add 1 to get month in range 1-12
+        const depDay = departureDate.getDate();
+        const formattedDepDate = `${depYear}-${depMonth.toString().padStart(2, '0')}-${depDay.toString().padStart(2, '0')}`;
+
+        const returnDate = new Date(props.returnDate);
+        const returnYear = returnDate.getFullYear();
+        const returnMonth = returnDate.getMonth() + 1; // add 1 to get month in range 1-12
+        const returnDay = returnDate.getDate();
+        const formattedReturnDate = `${returnYear}-${returnMonth.toString().padStart(2, '0')}-${returnDay.toString().padStart(2, '0')}`;
+        // const returnYear = returnDate.getFullYear();
+
         // console.log('getFlight')
         departureAirports.forEach(departureAirport => {
             destAirports.forEach((destAirport, index) => {
                 setTimeout(() => {
                     console.log(destAirport.AirportCode + " " + departureAirport.AirportCode)
-                    fetch(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${departureAirport.AirportCode}&destinationLocationCode=${destAirport.AirportCode}&departureDate=${formattedDate}&adults=1&nonStop=false&currencyCode=CAD&max=1`, {
+                    fetch(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${departureAirport.AirportCode}&destinationLocationCode=${destAirport.AirportCode}&departureDate=${formattedDepDate}&adults=1&nonStop=false&currencyCode=CAD&max=1`, {
                         headers: {
                             'Authorization': `Bearer ${props.amadeusAccessToken}`
                         }
@@ -181,6 +218,33 @@ export default function DestinationSelectedScreen(props) {
         }, null);
         console.log(cheapestFlightTemp)
         setCheapestFlight(cheapestFlightTemp)
+        if (cheapestFlightTemp !== null) {
+            const depAirportCheapest = cheapestFlightTemp.data[0].itineraries[0].segments[0].departure.iataCode
+            const segments = cheapestFlight.data[0].itineraries[0].segments;
+            const destAirportCheapest = cheapestFlightTemp.data[0].itineraries[0].segments[segments.length - 1].arrival.iataCode
+            fetch(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${destAirportCheapest}&destinationLocationCode=${depAirportCheapest}&departureDate=${formattedReturnDate}&adults=1&nonStop=false&currencyCode=CAD&max=1`, {
+                headers: {
+                    'Authorization': `Bearer ${props.amadeusAccessToken}`
+                }
+            })
+                .then(response => response.json())
+                .then(response => {
+                    console.log(depAirportCheapest + " " + destAirportCheapest + " ")
+                    // console.log(response)
+                    if (response.meta.count != 0) {
+                        console.log("HERE")
+                        setReturnFlight(response)
+                        console.log(response)
+                    }
+                })
+                .catch(error => {
+                    if (error.message !== "TypeError: undefined is not an object (evaluating 'data.meta.count')") {
+                        console.error(error.message);
+                    } else {
+                        console.log(error.message)
+                    }
+                })
+        }
     }
 
     useEffect(() => {
