6e299c0d426f121877bcd5a49b09ea975fb025cc
maanikg <gognamaanik@gmail.com>
2023-08-15 11:50:04 -0400

    scikitlearn clustering

--- test-router/python/flaskTest.py
+++ test-router/python/flaskTest.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 
@@ -8,17 +10,40 @@ app = Flask(__name__)
 @app.route("/api/data", methods=["POST"])
 def get_data():
     # Get the data to return
-    body = request.data
-    # data = request.json
-    # attractions = data[0]["attractions"]
-    print(body)
-    return "OK"
-    # data = request.json['attractions']
-    # data=request
-    # print(data)
+    # body = request.data[0]
+    attractions = request.get_json()
+    locData = [
+        [attraction["latitude"], attraction["longitude"]] for attraction in attractions
+    ]
+    # print(locData)
+    # return locData
 
-    # Return the data in JSON format
-    # return
+    X = np.array(locData)
+    kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+    labels = kmeans.labels_
+
+    # Return the cluster labels as part of the response
+    response = [
+        {"data": locData[i], "label": int(labels[i])} for i in range(len(locData))
+    ]
+
+
+    print(response)
+    return jsonify(response)
+    print(data)
+    for i in data:
+        print(str(i["latitude"]) + " " + str(i["longitude"]))
+    # attractions = data['attractions']
+    # print(attractions)
+    # locations = []
+    # for attraction in attractions:
+    #     latitude = attraction['latitude']
+    #     longitude = attraction['longitude']
+    #     locations.append({'latitude': latitude, 'longitude': longitude})
+
+    # Return the locations in JSON format
+    # print(data)
+    return data
 
 
 if __name__ == "__main__":
